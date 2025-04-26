d9308c7d8fe393d97f54fdf699da094e2435df14
Aayush Bindal <176171584+Aayush-Bindal@users.noreply.github.com>
2025-04-26 21:35:56 +0530

    Delete backend directory

--- backend/oauth2/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import GoogleLoginView, GoogleAuthCallbackView
-
-app_name = "oauth2"
-
-urlpatterns = [
-    path("login/", GoogleLoginView.as_view(), name="login"),
-    path("google/callback/", GoogleAuthCallbackView.as_view(), name="callback"),
-]
